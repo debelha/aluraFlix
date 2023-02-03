f5e9f7b4c004481f0deb0742dcdd246ebb7510a7
Deborah Leite <dleitedias@gmail.com>
2023-02-02 23:10:05 -0300

    css + js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var comedyMovie = "https://imgs.search.brave.com/tN51s2iOnRR7tyvO5eX3h7pF0fuEnZyjTifrjaPKJyw/rs:fit:541:800:1/g:ce/aHR0cHM6Ly93d3cu/Y2xpY2tncmF0aXMu/Y29tLmJyL191cGxv/YWQvbW92aWVzLzIw/MTMvMDkvMjcvZmFt/aWxpYS1kby1iYWd1/bGhvLmpwZw";
+
+var cartoon = "https://imgs.search.brave.com/tptPl_0p7epM8iOWAk8O8HyJggkqw6FdyX4i-Sx48Uw/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9lcy53/ZWIuaW1nMi5hY3N0/YS5uZXQvcGljdHVy/ZXMvMjEvMDMvMzAv/MTYvMTYvMDQ1NTAz/My5qcGc";
+
+var serie = "https://imgs.search.brave.com/vLAA8GLnxqC43FJ3icQ7WK2aaR328oCh6pKzmZSkNfQ/rs:fit:900:1200:1/g:ce/aHR0cHM6Ly93d3cu/Y2luZW1hc2NvbWlj/cy5jb20vd3AtY29u/dGVudC91cGxvYWRz/LzIwMjAvMDEvMTdi/NTY0OGRjOTdjMjEx/ZjJkODViMWQ5MWI0/MGRjZTAuanBn";
+
+document.write("<img src=" + comedyMovie + ">");
+document.write("<img src=" + cartoon + ">");
+document.write("<img src=" + serie + ">");
